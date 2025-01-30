b076a3c86c264103f6d935d8d5f40ccac3f531dc
Kathleen Hamrick <kchamrick@gmail.com>
2025-01-30 12:23:43 -0500

    Thursday updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
+import React, {useState} from 'react';
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
 
+function App() {
+const [token, setToken] = useState(null);
   return (
-    <>App</>
+    <>
+    <h2>Token: {token}</h2>
+    <SignUpForm setToken={setToken} />
+    <Authenticate token={token} />
+    </>
   )
 }
 
